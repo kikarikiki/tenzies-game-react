0f489bf3f16d9af18ab7267c5c777e41d8d88028
Antje Stiller <antjestiller@gmx.de>
2023-10-07 14:51:37 +0200

    turn dice to objects

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Die from "./components/Die"
+import {nanoid} from "nanoid"
 
 export function App() {
 
@@ -8,15 +9,16 @@ export function App() {
     let randomNumbers = [];
     for (let i = 0; i < 10; i++) {
         let randomNumber = Math.ceil(Math.random() * 6)
-        randomNumbers.push(randomNumber)
+        // in order for each Dice to contain information if its being held turn into object
+        randomNumbers.push({ value: randomNumber, isHeld: false, id: nanoid() })
     }
       return randomNumbers
     }
 
-    // State that holds values of Dice
+    // State that holds values/informations of Dice
     const [valuesDice, setValueDice] = React.useState(allNewDice)
     // generate Die elements + their value & render to screen
-    const valueDice = valuesDice.map(num => <Die value={num} />)
+    const valueDice = valuesDice.map(num => <Die value={num.value} key={num.id} />)
 
     // Handle Roll Dice
     function rollDice() {
