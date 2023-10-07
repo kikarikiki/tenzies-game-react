c978018076a5dad190fd97c1d12bbec8ce6ff7ac
Antje Stiller <antjestiller@gmx.de>
2023-10-07 10:55:33 +0200

    generate randNumsArr, map arr to Die components, Roll dice Btn, Style Roll dice Btn

--- src/app.jsx
+++ src/app.jsx
@@ -3,22 +3,34 @@ import Die from "./components/Die"
 
 export function App() {
 
+  // random Die values
+  function allNewDice() {
+    let randomNumbers = [];
+    for (let i = 0; i < 10; i++) {
+        let randomNumber = Math.ceil(Math.random() * 6)
+        randomNumbers.push(randomNumber)
+    }
+      return randomNumbers
+    }
+
+    // State that holds values of Dice
+    const [valuesDice, setValueDice] = React.useState(allNewDice)
+    // generate Die elements + their value & render to screen
+    const valueDice = valuesDice.map(num => <Die value={num} />)
+
+    // Handle Roll Dice
+    function rollDice() {
+      setValueDice(allNewDice)
+    }
+
   return (
     <>
       <div className="wrapper">
         <main>
           <div className="dice-container">
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
-            <Die value="1" />
+            {valueDice}
           </div>
+          <button type="button" className="btn" onClick={rollDice}>Roll</button>
         </main>
       </div>
     </>
