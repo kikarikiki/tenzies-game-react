90e65ea16a1047478e1cc5d2e49c8a0586326759
Antje Stiller <antjestiller@gmx.de>
2023-10-09 11:04:57 +0200

    update rollDice() to roll dice that aren't .isHeld === true, create HelperFunction generateNewDice()

--- src/app.jsx
+++ src/app.jsx
@@ -4,48 +4,64 @@ import {nanoid} from "nanoid"
 
 export function App() {
 
+  // State that holds values/informations of Dice
+  const [valuesDice, setValueDice] = React.useState(allNewDice())
+
+  // Helper Function
+  function generateNewDie() {
+    return {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+    }
+  }
+
   // random Die values
   function allNewDice() {
-    let randomNumbers = [];
+    const randomNumbers = [];
     for (let i = 0; i < 10; i++) {
-        let randomNumber = Math.ceil(Math.random() * 6)
         // in order for each Dice to contain information if its being held turn into object
-        randomNumbers.push({ value: randomNumber, isHeld: false, id: nanoid() })
-    }
+        randomNumbers.push(generateNewDie())
+      }
       return randomNumbers
     }
 
-    // State that holds values/informations of Dice
-    const [valuesDice, setValueDice] = React.useState(allNewDice)
-    // generate Die elements + their value & render to screen
-    const valueDice = valuesDice.map(num => (
-      <Die
-        value = {num.value}
-        key = {num.id}
-        isHeld = {num.isHeld}
-        holdDice={() => holdDice(num.id)}
-      />
-    ))
-
     // Handle Roll Dice
     function rollDice() {
-      setValueDice(allNewDice)
+      setValueDice(oldDice => oldDice.map(die => {
+        return die.isHeld ?
+            die :
+            generateNewDie()
+      }))
     }
 
-    // Handle Hold Dice
-    function holdDice(id) {
-      // Flip .isHeld on the object that was clicked based on id
-      setValueDice(oldDice => oldDice.map(die => {
-          return die.id === id ?
-              {...die, isHeld: !die.isHeld} :
-              die
-      }))
+  // generate Die elements + their value & render to screen
+  const valueDice = valuesDice.map(num => (
+    <Die
+      value = {num.value}
+      key = {num.id}
+      isHeld = {num.isHeld}
+      holdDice={() => holdDice(num.id)}
+    />
+  ))
+
+
+  // Handle Hold Dice
+  function holdDice(id) {
+    // Flip .isHeld on the object that was clicked based on id
+    setValueDice(oldDice => oldDice.map(die => {
+        return die.id === id ?
+            {...die, isHeld: !die.isHeld} :
+            die
+    }))
   }
 
   return (
     <>
       <div className="wrapper">
         <main>
+          <h1 className="title">Tenzies</h1>
+          <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <div className="dice-container">
             {valueDice }
           </div>
